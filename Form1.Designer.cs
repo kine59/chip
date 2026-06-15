c88eb019718ab83808bc96b69f67b2f155a2d776
kine59 <159332722+kine59@users.noreply.github.com>
2026-06-14 22:01:54 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Cookie_Clicker
+{
+    partial class ChipGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChipGame));
+            this.pictureboxCookie = new System.Windows.Forms.PictureBox();
+            this.menustripMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelClicks = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxCookie)).BeginInit();
+            this.menustripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureboxCookie
+            // 
+            this.pictureboxCookie.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureboxCookie.Image = global::Cookie_Clicker.Properties.Resources.Cookie;
+            this.pictureboxCookie.Location = new System.Drawing.Point(83, 37);
+            this.pictureboxCookie.Name = "pictureboxCookie";
+            this.pictureboxCookie.Size = new System.Drawing.Size(125, 125);
+            this.pictureboxCookie.TabIndex = 0;
+            this.pictureboxCookie.TabStop = false;
+            this.pictureboxCookie.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // menustripMain
+            // 
+            this.menustripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menustripMain.Location = new System.Drawing.Point(0, 0);
+            this.menustripMain.Name = "menustripMain";
+            this.menustripMain.Size = new System.Drawing.Size(298, 24);
+            this.menustripMain.TabIndex = 1;
+            this.menustripMain.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(104, 6);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // labelClicks
+            // 
+            this.labelClicks.AutoSize = true;
+            this.labelClicks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClicks.Location = new System.Drawing.Point(12, 176);
+            this.labelClicks.Name = "labelClicks";
+            this.labelClicks.Size = new System.Drawing.Size(58, 20);
+            this.labelClicks.TabIndex = 2;
+            this.labelClicks.Text = "Clicks: ";
+            // 
+            // ChipGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(298, 214);
+            this.Controls.Add(this.labelClicks);
+            this.Controls.Add(this.pictureboxCookie);
+            this.Controls.Add(this.menustripMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menustripMain;
+            this.Name = "ChipGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chip\'";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxCookie)).EndInit();
+            this.menustripMain.ResumeLayout(false);
+            this.menustripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureboxCookie;
+        private System.Windows.Forms.MenuStrip menustripMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.Label labelClicks;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cookie_Clicker
+{
+    public partial class ChipGame : Form
+    {
+        public ChipGame()
+        {
+            InitializeComponent();
+        }
+
+        int clicks = 0;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            clicks += 1; // this increments the clicks by 1
+            Form1_Load(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelClicks.Text = "Clicks: " + clicks.ToString();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveclicks = new SaveFileDialog();
+            saveclicks.Filter = "CC Save|*.cc";
+            DialogResult result = saveclicks.ShowDialog();
+
+
+            if (DialogResult.OK == result)
+            {
+                File.WriteAllText(saveclicks.FileName, clicks.ToString());
+            }
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openclicks = new OpenFileDialog();
+            openclicks.Filter = "CC Save|*.cc";
+            DialogResult result = openclicks.ShowDialog();
+
+            if (result == DialogResult.OK) 
+            {
+                clicks = 0;
+                clicks = int.Parse(File.ReadAllText(openclicks.FileName));
+                labelClicks.Text = "Clicks: " + clicks.ToString();
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chip' by Kine59\nv1.0.0\n\n" +
+                "Licensed under the Apache License", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
